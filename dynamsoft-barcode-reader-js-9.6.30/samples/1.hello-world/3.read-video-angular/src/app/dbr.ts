7faed58b646f2f7332eb91efc2549f885846259a
jesusrh <jesusrh@comfama.com.co>
2023-09-25 13:24:54 -0500

    Se agrega sdk dynamsoft

---
+++ dynamsoft-barcode-reader-js-9.6.30/samples/1.hello-world/3.read-video-angular/src/app/dbr.ts
@@ -0,0 +1,16 @@
+import { BarcodeReader } from 'dynamsoft-javascript-barcode';
+
+/** LICENSE ALERT - README
+ * To use the library, you need to first specify a license key using the API "license" as shown below.
+ */
+
+BarcodeReader.license = 'DLS2eyJoYW5kc2hha2VDb2RlIjoiMTAyMjQ0MDA2LVRYbFhaV0pRY205cVgyUmljZyIsIm1haW5TZXJ2ZXJVUkwiOiJodHRwczovL21kbHMuZHluYW1zb2Z0b25saW5lLmNvbS8iLCJvcmdhbml6YXRpb25JRCI6IjEwMjI0NDAwNiIsInN0YW5kYnlTZXJ2ZXJVUkwiOiJodHRwczovL3NkbHMuZHluYW1zb2Z0b25saW5lLmNvbS8iLCJjaGVja0NvZGUiOjIwNzA4ODU5NDN9';
+
+/**
+ * You can visit https://www.dynamsoft.com/customer/license/trialLicense?utm_source=zip&product=dbr&package=js to get your own trial license good for 30 days.
+ * Note that if you downloaded this sample from Dynamsoft while logged in, the above license key may already be your own 30-day trial license.
+ * For more information, see https://www.dynamsoft.com/barcode-reader/programming/javascript/user-guide/?ver=9.6.30&utm_source=zip#specify-the-license or contact support@dynamsoft.com.
+ * LICENSE ALERT - THE END
+ */
+
+BarcodeReader.engineResourcePath = 'https://cdn.jsdelivr.net/npm/dynamsoft-javascript-barcode@9.6.30/dist/';
